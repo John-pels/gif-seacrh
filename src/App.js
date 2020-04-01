c4c36e09efa7e34b811f8f30520b80179b445833
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2020-04-02 00:26:07 +0100

    I add React route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import Homepage from "./Homepage";
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Switch>
+        <Route path="/" component={Homepage} />
+      </Switch>
     </div>
   );
 }
