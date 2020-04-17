85d73fb35da5788ede5e14aa6e0288ce21180035
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2020-04-17 19:29:31 +0100

    I move the API params to the config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,6 @@
 export const API_KEY = "OYa73aJOBfbenMLiAav9ZXswodSdoTTW";
 export const URL = "https://api.giphy.com/v1/gifs/search?api_key=";
+export const LIMIT = "limit=30";
+export const RATING = "rating=G";
+export const OFFSET = "offset=0";
+export const LANG = "lang=en";
